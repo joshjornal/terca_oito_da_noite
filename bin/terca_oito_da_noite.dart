0d7f9ae5f33a4f5d4be15a6dc6aa0a78ed30dde5
joshjornal <143331209+joshjornal@users.noreply.github.com>
2023-08-29 20:46:38 -0300

    Initial commit

---
+++ bin/terca_oito_da_noite.dart
@@ -0,0 +1,5 @@
+import 'package:terca_oito_da_noite/terca_oito_da_noite.dart' as terca_oito_da_noite;
+
+void main(List<String> arguments) {
+  print('Hello world: ${terca_oito_da_noite.calculate()}!');
+}
---
+++ test/terca_oito_da_noite_test.dart
@@ -0,0 +1,8 @@
+import 'package:terca_oito_da_noite/terca_oito_da_noite.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
